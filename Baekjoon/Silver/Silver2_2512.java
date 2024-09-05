6bbecff242c8cdfdbd9c58c6090486401448a259
HyominAn0401 <slhm7547@naver.com>
2024-09-05 23:35:06 +0900

    Upload Silver2_2512

---
+++ Baekjoon/Silver/Silver2_2512.java
@@ -0,0 +1,72 @@
+package Silver.Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Silver2_2512 {
+
+    public static int N;
+    public static int [] budget;
+    public static void main(String[] args) throws Exception{
+
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            N = Integer.parseInt(br.readLine());
+            budget = new int[N];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++){
+                budget[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int nationalBudget = Integer.parseInt(br.readLine());
+
+            Arrays.sort(budget);
+            int sum = 0;
+            for(int i=0; i<N; i++){
+                sum += budget[i];
+            }
+
+            int [] maximumPrice = new int[budget[N-1]-budget[0]+1];
+            int j = 0;
+            for(int i=budget[0]; i<=budget[N-1]; i++){
+                maximumPrice[j] = i;
+                j++;
+            }
+
+            System.out.println(sum<= nationalBudget? budget[N-1]: binarySearch(maximumPrice, nationalBudget));
+
+        }
+
+
+    }
+
+    public static int binarySearch(int [] array, int nationalBudget){
+        int start = 0;
+        int end = array.length-1;
+        int sum = 0;
+        int answer = 0;
+
+        while(start < end){
+            int mid = (start+end)/2;
+            answer = array[mid];
+
+            for(int i=0; i<N; i++){
+                if(budget[i] >= answer){
+                    sum += answer;
+                }
+                else{
+                    sum += budget[i];
+                }
+            }
+
+            if(sum > nationalBudget) {
+                end = mid - 1;
+            }
+            else start = mid+1;
+        }
+
+        return answer;
+    }
+}

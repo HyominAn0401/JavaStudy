bff5ffed3485d4c72592aa267d67114ca5a5b90a
HyominAn0401 <slhm7547@naver.com>
2024-10-05 23:48:05 +0900

    Resolved merge conflicts

--- Baekjoon/Silver/Silver2_11053.java
+++ Baekjoon/Silver/Silver2_11053.java
@@ -1,4 +1,40 @@
 package Silver;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Silver2_11053 {
+    public static void main(String[] args) throws Exception{
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            int N = Integer.parseInt(br.readLine());
+
+            int [] num = new int[N];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++){
+                num[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int current = num[0];
+            int maxCurrent = current;
+            int cnt = 1;
+            int maxCnt = cnt;
+
+            for(int i=1; i<N; i++){
+                if(current >= num[i]){
+                    current = num[i];
+                }
+                else{
+                    current = current + num[i];
+                    cnt++;
+                }
+                maxCurrent = Math.max(maxCurrent, current);
+                maxCnt = Math.max(maxCnt, cnt);
+            }
+
+            System.out.println(maxCnt);
+
+        }
+    }
 }

2a608306ae20984ca230d031f631c646963b17c4
HyominAn0401 <slhm7547@naver.com>
2025-01-23 22:44:33 +0900

    14568 with comments

--- Baekjoon/Bronze/Bronze3_14568.java
+++ Baekjoon/Bronze/Bronze3_14568.java
@@ -13,23 +13,32 @@ public class Bronze3_14568 {
         // 4. b는 1부터 시작하고 N보다 작은 값들을 넣어주면서 반복
         // 5. a는 (N-c)-j값, a-2가 b보다 크거나 같으면 조건에 만족하므로 경우의 수 증가
         // 6. 반복문 종료 시 결과값 출력
+
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            // 사탕의 개수 N 입력받기
             int N = Integer.parseInt(br.readLine());
+            // 3명에게 나눠줄 사탕 개수를 저장할 변수
             int a=0, b=0, c=0;
 
+            // 경우의 수를 저장할 변수
             int count = 0;
+            // c에 N보다 작은 짝수를 넣어가며 반복
             for(int i=2; i<N; i+=2){
                 c = i;
+                // a+b가 4보다 작으면 종료
                 if(N-c < 4){
                     break;
                 }
+                // b에 1부터 N보다 작은 수를 넣어가며 반복
                 for(int j=1; j<N; j++){
                     b = j;
                     a = (N-c)-j;
+                    // a-2>= b 조건에 만족하면 경우의 수 증가
                     if(a-2 >= b)
                         count++;
                 }
             }
+            // 결과값 출력
             System.out.println(count);
         }
     }

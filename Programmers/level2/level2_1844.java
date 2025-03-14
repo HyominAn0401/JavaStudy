b71c217dc140d4102a042bbdc6d8b2ba44e6ebd5
HyominAn0401 <slhm7547@naver.com>
2025-03-14 23:58:57 +0900

    Programmers 1844

--- Programmers/level2/level2_1844.java
+++ Programmers/level2/level2_1844.java
@@ -7,7 +7,13 @@ public class level2_1844 {
     static int [] dx = {-1, 1, 0, 0};
     static int [] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws Exception{
-        int [][] maps = {{1,0,1,1,1}, {1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1}, {0,0,0,0,1}};
+        int [][] maps = {
+                {1,0,1,1,1},
+                {1,0,1,0,1},
+                {1,0,1,1,1},
+                {1,1,1,0,1},
+                {0,0,0,0,1}
+        };
         System.out.println(solution(maps));
     }
 
@@ -18,25 +24,29 @@ public class level2_1844 {
         boolean [][] visited = new boolean[n][m];
 
         Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0,0));
+        queue.add(new Node(0,0, 1));
         visited[0][0] = true;
-        int sum = 0;
 
         while(!queue.isEmpty()){
             Node node = queue.poll();
             int x = node.getX();
             int y = node.getY();
+            int count = node.getCount();
+
+            if(x==n-1 && y==m-1){
+                return count;
+            }
 
             for(int i=0; i<4; i++){
                 int nx = x+dx[i];
                 int ny = y+dy[i];
-                if(nx>=0 && ny>=0 && nx<n && ny<n){
+
+                if(nx>=0 && ny>=0 && nx<n && ny<m){
                     if(!visited[nx][ny] && maps[nx][ny]==1){
-                        queue.add(new Node(nx, ny));
-                        sum += maps[nx][ny];
+                        queue.add(new Node(nx, ny, count+1));
+                        visited[nx][ny] = true;
                     }
                 }
-                result = Math.min(sum, result);
             }
         }
 
@@ -48,9 +58,11 @@ public class level2_1844 {
 class Node{
     private int x;
     private int y;
-    public Node(int x, int y){
+    private int count;
+    public Node(int x, int y, int count){
         this.x = x;
         this.y = y;
+        this.count = count;
     }
 
     public int getX(){
@@ -59,5 +71,9 @@ class Node{
 
     public int getY(){
         return y;
+    }
+
+    public int getCount(){
+        return count;
     }
 }

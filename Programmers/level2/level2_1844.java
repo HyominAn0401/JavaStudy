385ad50535751e59a160ecae3667fe9bf43b60b0
HyominAn0401 <slhm7547@naver.com>
2025-03-14 23:47:03 +0900

    1844

---
+++ Programmers/level2/level2_1844.java
@@ -0,0 +1,63 @@
+package level2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class level2_1844 {
+    static int [] dx = {-1, 1, 0, 0};
+    static int [] dy = {0, 0, -1, 1};
+    public static void main(String[] args) throws Exception{
+        int [][] maps = {{1,0,1,1,1}, {1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1}, {0,0,0,0,1}};
+        System.out.println(solution(maps));
+    }
+
+    public static int solution(int [][] maps){
+        int result = -1;
+        int n= maps.length;
+        int m = maps[0].length;
+        boolean [][] visited = new boolean[n][m];
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0,0));
+        visited[0][0] = true;
+        int sum = 0;
+
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            int x = node.getX();
+            int y = node.getY();
+
+            for(int i=0; i<4; i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                if(nx>=0 && ny>=0 && nx<n && ny<n){
+                    if(!visited[nx][ny] && maps[nx][ny]==1){
+                        queue.add(new Node(nx, ny));
+                        sum += maps[nx][ny];
+                    }
+                }
+                result = Math.min(sum, result);
+            }
+        }
+
+        return result;
+    }
+
+
+}
+class Node{
+    private int x;
+    private int y;
+    public Node(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+}

30ab455884d582219f2dd704f582d754a254b85b
HyominAn0401 <slhm7547@naver.com>
2025-05-06 22:50:32 +0900

    42888

--- Programmers/Kakao/BlindRecruitment2019/KakaoBlindRecruitment2019_42888.java
+++ Programmers/Kakao/BlindRecruitment2019/KakaoBlindRecruitment2019_42888.java
@@ -8,11 +8,13 @@ import java.util.Map;
 
 public class KakaoBlindRecruitment2019_42888 {
 
+    // User 클래스
     static class User{
         private String uid;
         private String nickname;
         private boolean isIn;
 
+        // 생성자
         public User(String uid, String nickname, boolean isIn){
             this.uid = uid;
             this.nickname = nickname;
@@ -22,6 +24,7 @@ public class KakaoBlindRecruitment2019_42888 {
 
     public static void main(String[] args) throws Exception{
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            // 기록
             String record [] = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
 
             for(String str : solution(record)){
@@ -31,26 +34,38 @@ public class KakaoBlindRecruitment2019_42888 {
     }
 
     public static String[] solution(String[] record){
+        // User 객체 저장 ArrayList
         ArrayList<User> userArrayList = new ArrayList<>();
+        // 닉네임 저장 Map
         Map<String, String> nicknameMap = new HashMap<>();
         String [] str = new String[record.length];
 
+        // record 길이만큼 반복
         for(int i=0; i<record.length; i++){
+            // 문자열 분리
             String [] order = record[i].split(" ");
+            // 행동
             String behavior = order[0];
 
             str[i] = behavior;
 
             String uid = order[1];
+            // 행동 분류
             switch (behavior) {
+                // 입장인 경우
                 case "Enter":
+                    // 닉네임
                     String nickname = order[2];
+                    // 추가
                     nicknameMap.put(uid, nickname);
+                    // 입장으로 추가
                     userArrayList.add(new User(uid, nickname, true));
                     break;
+                    // 나간 경우
                 case "Leave":
                     // 찾기
                     for(User user: userArrayList){
+                        // uid가 같고 있는 경우
                         if(user.uid.equals(uid) && user.isIn){
                             user.isIn = false;
                         }
@@ -58,7 +73,9 @@ public class KakaoBlindRecruitment2019_42888 {
                     // 조건 : isIn이 true인 경우만
                     // 찾은 객체의 isIn = false로 변경
                     break;
+                    // 상태 변경
                 case "Change":
+                    // 바꾼 이름
                     String changedName = order[2];
                     nicknameMap.put(uid, changedName);
                     // 찾기
@@ -72,9 +89,11 @@ public class KakaoBlindRecruitment2019_42888 {
             }
         }
 
+        // 정답 ArrayList
         ArrayList<String> answer = new ArrayList<>();
 
         for(int i=0; i<record.length; i++){
+            // 입력받기
             StringBuilder sb = new StringBuilder();
             String uid = record[i].split(" ")[1];
 
@@ -92,6 +111,7 @@ public class KakaoBlindRecruitment2019_42888 {
             }
         }
 
+        // 반환
         return answer.toArray(new String[0]);
     }
 

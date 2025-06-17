9cbb387f9b90d8a2204cf7c4358b032a8d80be25
HyominAn0401 <slhm7547@naver.com>
2025-06-17 23:16:50 +0900

    1018

--- Baekjoon/Silver/Silver2_13908.java
+++ Baekjoon/Silver/Silver2_13908.java
@@ -1,4 +1,13 @@
 package Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 // brute force
 public class Silver2_13908 {
+    public static void main(String[] args) throws Exception{
+        try(BufferedReader br= new BufferedReader(new InputStreamReader(System.in))){
+
+        }
+    }
 }

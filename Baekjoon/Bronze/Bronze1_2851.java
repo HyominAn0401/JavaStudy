537afc2e93511418b988094ee9241aae46dcebcf
HyominAn0401 <slhm7547@naver.com>
2025-06-21 22:22:12 +0900

    Unsolved 2851

---
+++ Baekjoon/Bronze/Bronze1_2851.java
@@ -0,0 +1,14 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bronze1_2851 {
+    public static void main(String[] args) throws Exception{
+        try(BufferedReader br= new BufferedReader(new InputStreamReader(System.in))){
+            for(int i=0; i<10; i++){
+
+            }
+        }
+    }
+}

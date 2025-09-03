927ddc7951cfd146b64c9106d7d3945f7a261369
HyominAn0401 <slhm7547@naver.com>
2025-09-03 21:37:06 +0900

    14502

--- Baekjoon/Gold/Gold4_14502.java
+++ Baekjoon/Gold/Gold4_14502.java
@@ -5,10 +5,12 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Gold4_14502 {
+    // x, y축 이동 벡터
     static int [] dx = {-1, 1, 0, 0};
     static int [] dy = {0, 0, -1, 1};
     static int N, M;
     static int [][] map;
+    // 방문 여부 확인 이차원 배열
     static boolean [][] visited;
 
     public static void main(String[] args) throws Exception{
@@ -17,6 +19,7 @@ public class Gold4_14502 {
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
 
+            // 이차원 배열 초기화
             map = new int[N][M];
             visited = new boolean[N][M];
 
